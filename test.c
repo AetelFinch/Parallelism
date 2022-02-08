13fbb1e0e9d60e99acc84f221c9ae52e0ff602da
AetelFinch <maxchup30@gmail.com>
2022-02-08 11:49:33 +0700

    rewrite to parallel computing

--- test.c
+++ test.c
@@ -6,12 +6,16 @@ int main()
 {
 	int n_sin = 10000000;
 	double *sins = (double*)malloc(sizeof(double) * n_sin);
+
+	#pragma acc parallel
 	for (int i = 0; i < n_sin + 1; i++)
 	{
 		sins[i] = sin(i);
 	}
 
 	double sum_sin = 0;
+
+	#pragma acc parallel
 	for (int i =0; i < n_sin + 1; i++)
 	{
 		sum_sin += sins[i];
